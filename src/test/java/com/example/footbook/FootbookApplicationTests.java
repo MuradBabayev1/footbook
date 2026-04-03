2ff983e2447ef7cc85a03f648c637e9f36020017
user <user@Murads-MacBook-Air.local>
2026-04-03 23:16:57 +0400

    Initial commit: FootBook Spring Boot project

---
+++ src/test/java/com/example/footbook/FootbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
