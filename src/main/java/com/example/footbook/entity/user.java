72f23a723f3e3e0a4893b8bf6af930303f83784c
Murad <muradb836@gmail.com>
2026-04-03 23:48:56 +0400

    test

---
+++ src/main/java/com/example/footbook/entity/user.java
@@ -0,0 +1,25 @@
+package com.example.footbook.entity;
+
+import org.apache.commons.math3.analysis.function.Identity;
+
+Identity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Booking> bookings;
+}
